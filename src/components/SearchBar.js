7f38717dbeef6bff95991121981d6dae7e9d61a4
Safi-Ullah <safiullahmughal@gmail.com>
2018-10-14 18:54:06 +0500

    searchbar and styling changed a little

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import { Navbar, FormGroup, FormControl,
-         Button } from 'react-bootstrap'
+import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
 
-
-class SearchBar extends React.Component
-{
-    render()
-    {
+class SearchBar extends React.Component {
+    render() {
         return (
-            <form onSubmit={this.search}>
-                <Navbar.Form>
-                    <FormGroup>
-                        <FormControl type="text" onChange={this.props.setQuery}
-                         value={this.props.query} placeholder="Search" />
-                    </FormGroup>
-                    {' '}
-                    <Button type="submit" onClick={this.props.search}>Search</Button>
-                </Navbar.Form>
+            <form onSubmit={this.props.search}>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl type="text" onChange={this.props.setQuery} />
+                        <InputGroup.Button>
+                            {/* <Glyphicon glyph="search" /> */}
+                            <Button type='submit'>Search</Button>
+                        </InputGroup.Button>
+                    </InputGroup>
+                </FormGroup>
             </form>
         );
     }
--- src/components/YoutubeApp.js
+++ src/components/YoutubeApp.js
@@ -3,10 +3,8 @@ import SearchList from './SearchList';
 import YoutubePlayer from './YoutubePlayer';
 import { Row, Col } from 'react-bootstrap'
 
-class YoutubeApp extends React.Component
-{
-    constructor(props)
-    {
+class YoutubeApp extends React.Component {
+    constructor(props) {
         super(props);
         this.setVideoDetails = this.setVideoDetails.bind(this);
         this.state = {
@@ -24,33 +22,36 @@ class YoutubeApp extends React.Component
         });
     }
 
-    render()
-    {
+    render() {
         return (
             <div>
                 <nav className='navbar navbar-default'>
                     <div className='container-fluid'>
                         <div className="navbar-header">
-                        <Row>
-                            <Col sm={12}>
-                                <a className="navbar-brand brand-name">MiniTube</a>
-                                <img alt='logo' className='logo' src={require('../images/logos/yt-logo.svg')}/>
-                            </Col>
-                        </Row>
+                            <Row>
+                                <Col sm={12}>
+                                    <a className="navbar-brand brand-name">MiniTube</a>
+                                    <img alt='logo' className='logo' src={require('../images/logos/yt-logo.svg')} />
+                                </Col>
+                            </Row>
                         </div>
                     </div>
                 </nav>
                 <div className='container-fluid'>
                     <Row>
-                        <Col md={7}>
-                            <YoutubePlayer title={this.state.videoTitle} desc={this.state.videoDesc}
-                                id={this.state.videoId} show={this.state.show}
-                                height={this.state.playerHeight} width={this.state.playerWidth}/>
-                        </Col>
-                        <Col md={5}>
+                        {
+                            this.state.show
+                                ? <Col sm={7}>
+                                    <YoutubePlayer title={this.state.videoTitle} desc={this.state.videoDesc}
+                                        id={this.state.videoId} show={this.state.show}
+                                        height={this.state.playerHeight} width={this.state.playerWidth} />
+                                </Col>
+                                : <Col sm={3} />
+                        }
+                        <Col sm={this.state.show ? 5 : 6}>
                             <SearchList setVideoDetails={this.setVideoDetails}
                                 items={this.state.items} nextPage={this.nextPage}
-                                prevPage={this.prevPage}/>
+                                prevPage={this.prevPage} />
                         </Col>
                     </Row>
                 </div>
